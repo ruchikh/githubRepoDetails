aed42d241f6da4bb3575b686b418cdbb043466da
Ruchi <ruchikharwar4@gmail.com>
2019-01-25 21:01:58 +0530

    github organization repo details

---
+++ githubDetails/precache-manifest.769c9ae7dd31f31e02477cd1cddf7401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db4edc3b3ee3973d12da",
+    "url": "/static/js/main.db4edc3b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "db4edc3b3ee3973d12da",
+    "url": "/static/css/main.d6416c5b.chunk.css"
+  },
+  {
+    "revision": "83bdebf02dddd7a19015c4719af46df9",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      value: '',
+      data: [],
+      repoDetails: null,
+    }
+  }
+
+  handleChange = (e) => {
+    var val = e.target.value;
+    this.setState({
+      value: val
+    })
+  }
+
+ handelrepo = repo => (e) => {
+  const url = `https://api.github.com/repos/${repo.full_name}/commits`;
+  fetch(url).then(res => res.json()).then(data => this.setState({
+    repoDetails: {name:repo.name, description: repo.description, commits: data.splice(0, 5)}
+  }))
+ }
+
+
+ handleSubmit = (e) => {
+  e.preventDefault();
+  fetch(`https://api.github.com/orgs/${this.state.value}/repos`).then(res => res.json()).then(d => this.setState({
+    data: d,
+    value: ''
+  }))
+ }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>GitHub Organization Repo Details</h1>
+        </header>
+        <div className="main-container">
+          <div>
+            <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} placeholder="Search Org" value={this.state.value} id="input"/>
+            </form>  
+          </div>       
+          <div className="main_list">
+            <div>
+            <h1>Repo List</h1>
+              {
+                this.state.data && this.state.data.map((val, i) => {
+                  return <h4 onClick={this.handelrepo(val)}>{val.full_name}</h4>
+                })
+              }
+            </div>
+
+            <div>
+            <h1>Repo Details</h1>
+              {this.state.repoDetails && (<h4>{this.state.repoDetails.name}</h4>)}
+              {
+               this.state.repoDetails && this.state.repoDetails.commits.map(val => {
+                  return (
+                  <div className="commits-list">
+                  <h3>Author Name: {val.author.login}</h3>
+                  <p><b>Commit Msg</b>: {val.commit.message}</p>
+                  </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
